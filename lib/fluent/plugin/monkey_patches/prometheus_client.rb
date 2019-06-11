894e10a35f19edf3d2489b66c3b29d3dbd70597b
Harry Maclean <harrymaclean@gocardless.com>
2019-06-11 09:07:05 +0100

    show label

--- lib/fluent/plugin/monkey_patches/prometheus_client.rb
+++ lib/fluent/plugin/monkey_patches/prometheus_client.rb
@@ -7,10 +7,9 @@ module Prometheus
     module Formats
       module Text
           def self.escape(string, format = :doc)
-            string.
-              to_s.
-              encode("UTF-8", undef: :replace, invalid: :replace).
-              gsub(REGEX[format], REPLACE)
+            string.to_s.gsub(REGEX[format], REPLACE)
+          rescue => e
+            raise "error escaping string: #{string.inspect} (#{e.to_s})\n#{e.backtrace.join('\n')}"
           end
       end
     end
